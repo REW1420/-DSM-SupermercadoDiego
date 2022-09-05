4a9dc045eb711220038d1f090f127cfc9557ba3e
Colocho10 <110870983+Colocho10@users.noreply.github.com>
2022-09-05 01:30:27 -0600

    Cambios 4-9-2022
    Cambios guardados

---
+++ src/main/kotlin/inventario.kt
@@ -0,0 +1,16 @@
+class inventario {
+    var producto = venta()
+    var cantidad = venta()
+
+
+
+}fun consultarInventario(producto:List<String>,cantidad:List<Int>){
+
+    println("Productos   Cantidad")
+
+    for ((i, producto) in producto.withIndex()){
+
+        println(producto+" - "+cantidad[i])
+    }
+
+}
